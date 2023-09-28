ca982336427fccb04a50ae853c65566fc430bedc
Leonardo Cherubino <96987724+LeonardoCherubino@users.noreply.github.com>
2023-09-27 22:24:40 -0300

    p

---
+++ Exercicio48.py
@@ -0,0 +1,18 @@
+## Exercício 04-08: Escreva um programa que leia dois números e que pergunte qual operação você deseja realizar.
+
+Num1 = int(input('Entre com o primeiro número. '))
+Num2 = int(input('Entre com o segundo número. '))
+Operacao = input('Entre com a operação desejada. +, -, *, / ')
+# Resultado = 0
+
+if Operacao == '+':
+    Resultado = Num1 + Num2
+elif Operacao == '-':
+    Resultado = Num1 - Num2
+elif Operacao == '*':
+    Resultado = Num1 * Num2
+elif Operacao == '/':
+    Resultado = Num1 / Num2
+else:
+    print('Você escolheu opção inválida. ')
+print('O resultado da opreração escolhida é {}'.format(Resultado))    
